2aafac38326c1558cd3b6e74b642b7ceb1cd17c5
Bob Dubois <bob@redhat.com>
2015-11-17 11:36:57 +0000

    demo app

---
+++ openshift/app-build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+cd $(dirname $0)
+. ../environment
+. utils
+
+oc create -f - <<EOF || true
+kind: ImageStream
+apiVersion: v1
+metadata:
+  name: bankingapp
+  labels:
+    service: bankingapp
+EOF
+
+oc create -f - <<EOF
+kind: BuildConfig
+apiVersion: v1
+metadata:
+  name: bankingapp
+  labels:
+    service: bankingapp
+spec:
+  triggers:
+  - type: Generic
+    generic:
+      secret: secret
+  - type: GitHub
+    github:
+      secret: secret
+  - type: ImageChange
+  strategy:
+    type: Source
+    sourceStrategy:
+      env:
+      - name: http_proxy
+        value: $PROXY
+      - name: https_proxy
+        value: $PROXY
+      from:
+        kind: ImageStreamTag
+        name: jboss-eap6-openshift:6.4
+        namespace: openshift
+  source:
+    type: Git
+    git:
+      uri: $GITURL
+      ref: master
+#      ref: Feature-SaveCustomerDetails
+  output:
+    to:
+      kind: ImageStreamTag
+      name: bankingapp:latest
+EOF
